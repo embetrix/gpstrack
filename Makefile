32a6796d97c49c81b6fc1c8117fb95ee311c6e71
Embexus <info@embexus.com>
2017-06-22 14:43:40 +0200

    Initial commit of gpstrack using minmea library

---
+++ Makefile
@@ -0,0 +1,33 @@
+# (C) Copyright 2017
+#
+# Ayoub Zaki, Embexus Embedded Systems Solutions, ayoub.zaki@embexus.com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of
+# the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307 USA
+#
+
+CFLAGS = -Wall -Wextra -Werror -std=c99
+CFLAGS += -D_POSIX_C_SOURCE=199309L -D_BSD_SOURCE -D_DEFAULT_SOURCE -D_DARWIN_C_SOURCE
+
+all:  gpstrack
+
+clean:
+	$(RM) gpstrack *.o
+
+gpstrack: gpstrack.o minmea.o
+
+minmea.o: minmea.c minmea.h
+
+.PHONY: all clean
---
+++ gpstrack.c
@@ -0,0 +1,53 @@
+/*
+ * (C) Copyright 2017
+ * Ayoub Zaki, Embexus Embedded Systems Solutions, ayoub.zaki@embexus.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "minmea.h"
+
+int main(int argc, char **argv)
+{
+    FILE *fp;
+    char line[MINMEA_MAX_LENGTH];
+
+    if (argc > 1)
+        fp = fopen(argv[1], "r");
+
+    if(!fp) {
+        printf("Error opening %s\n",argv[1]);
+        return(-1);
+    }
+
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        if (minmea_sentence_id(line, false) == MINMEA_SENTENCE_RMC){
+                struct minmea_sentence_rmc frame;
+                if (minmea_parse_rmc(&frame, line)) {
+                    printf( "floating point degree coordinates and speed: (%f,%f) %f\n",
+                            minmea_tocoord(&frame.latitude),
+                            minmea_tocoord(&frame.longitude),
+                            minmea_tofloat(&frame.speed));
+                }
+        }
+    }
+    fclose(fp);
+    return 0;
+}
