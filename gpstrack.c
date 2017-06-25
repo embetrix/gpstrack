7c7b2fc51f95b6639028b910c0e6f42855aef440
Embexus <info@embexus.com>
2017-06-25 21:14:00 +0200

    add publishing gps data to pubnub cloud

--- gpstrack.c
+++ gpstrack.c
@@ -18,44 +18,111 @@
  * MA 02111-1307 USA
  */
 
+#define _GNU_SOURCE
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <string.h>
 #include <ctype.h>
+#include <json.h>
+#include <pubnub.h>
+#include <pubnub-sync.h>
 
 #include "minmea.h"
+
+#define DEBUG 0
+#define debug(args ...) if (DEBUG) fprintf(stderr, args)
+#define STRINGIFY(...) #__VA_ARGS__
+
+#define PUBKEY "pub-c-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+#define SUBKEY "sub-c-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+#define CHANNEL "gps-location"
+#define TMOUT 10
+
+#define SPEED_MIN 1
+
+/* Knot to meter/s conversion*/
+#define KNT2MPS 0.51444444444
 
 int main(int argc, char **argv)
 {
     FILE *fp;
     char line[MINMEA_MAX_LENGTH];
     char time[40];
+
     struct minmea_sentence_rmc frame;
     struct timespec ts;
     struct tm* ptm;
-    
+    float speed = 0.0;
+    char *gps_json_string = NULL;
+    json_object *gps;  
+    enum json_tokener_error error = json_tokener_success;
+
+    static const char* const gps_data = STRINGIFY(
+    [
+     {
+	"latlng": [
+	 %f,
+	 %f
+	],
+	"speed": %f,
+	"time": "%s"
+     }
+    ]
+    );
+
     if (argc > 1)
         fp = fopen(argv[1], "r");
+    else {
+        printf("Usage: %s /dev/ttyACM0\n",argv[0]);
+        return(-1);
+    }
 
     if(!fp) {
         printf("Error opening %s\n",argv[1]);
         return(-1);
     }
 
+    struct pubnub_sync *sync = pubnub_sync_init();
+    struct pubnub *p = pubnub_init(PUBKEY, SUBKEY, &pubnub_sync_callbacks, sync);
+
+    /*parse only RMC sentences*/	
     while (fgets(line, sizeof(line), fp) != NULL) {
         if (minmea_sentence_id(line, false) == MINMEA_SENTENCE_RMC){
-                
+
                 if (minmea_parse_rmc(&frame, line)) {
                     minmea_gettime(&ts, &frame.date, &frame.time);
-                    ptm = localtime (&ts.tv_sec);
-                    strftime (time, sizeof (time), "%Y-%m-%d %H:%M:%S", ptm);
-                    printf( "Time: %s - Coordinates: (%f,%f) - Speed: %f\n", 
-                            time,
-                            minmea_tocoord(&frame.latitude),
-                            minmea_tocoord(&frame.longitude),
-                            minmea_tofloat(&frame.speed));
+                    ptm = localtime(&ts.tv_sec);
+                    strftime(time, sizeof (time), "%Y-%m-%d %H:%M:%S", ptm);
+
+                    /*convert speed from knot to mps*/
+                    speed = KNT2MPS*minmea_tofloat(&frame.speed);
+
+                    asprintf(&gps_json_string, gps_data, 
+                             minmea_tocoord(&frame.latitude), 
+                             minmea_tocoord(&frame.longitude), 
+                             speed, 
+                             time);
+
+                    gps = json_tokener_parse_verbose(gps_json_string, &error);
+
+                    /*printing the json object*/
+                    debug("gps json: %s\n",json_object_to_json_string(gps));
+
+                    /*publish only if taget moving*/
+                    if (speed > SPEED_MIN) {
+                        pubnub_publish(p,CHANNEL,gps,TMOUT,NULL,NULL);
+	                if (pubnub_sync_last_result(sync) != PNR_OK)
+	                    printf("pubnub publish error!\n");
+                   }
                 }
         }
     }
+    free(gps_json_string);
+    json_object_put(gps);
+    pubnub_done(p);
     fclose(fp);
     return 0;
 }
